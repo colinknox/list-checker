718894b30190f08f909aaeae697f42d415bbd52c
Ninny <neninny@proton.me>
2025-12-29 00:07:39 -0500

    fixed find_index function. simplified some of the other functions.

--- main.py
+++ main.py
@@ -6,19 +6,25 @@ air_list = ["air", "air", "air", "air", "air"]
 num_list = [1, 1, 1, 5, 1]
 
 def contains_item(items, target):
-    if target not in items:
-        return False
-    else:
-        return True
+    return target in items
+    
+    # if target not in items:
+    #     return False
+    # else:
+    #     return True
     
 def count_occurrences(items, target):
     counter = 0
     
     for current_item in items:
-        if current_item != target:
-            pass
-        else:
+        if current_item == target:
             counter += 1
+
+    # for current_item in items:
+    #     if current_item != target:
+    #         pass
+    #     else:
+    #         counter += 1
 
     return counter
 
@@ -28,12 +34,16 @@ def find_index(items, target):
             pass
         else:
             return i
+        
+    return -1
 
 def is_empty(items):
-    if not items:
-        return True
-    else:
-        return False
+    return not items
+    
+    # if not items:
+    #     return True
+    # else:
+    #     return False
 
 def all_same(items):
     first_item = items[0]
@@ -53,15 +63,15 @@ def all_same(items):
 # test2 = contains_item(oceans, "pacific")
 # test3 = contains_item(oceans, "the red ocean")
     
-## count_occurrences TEST    
+# # count_occurrences TEST    
 # test1 = count_occurrences(elements, "air")
 # test2 = count_occurrences(elements, "fire")
 # test3 = count_occurrences(elements, "earth")
 
-## find_index TEST    
-# test1 = find_index(elements, "water")
-# test2 = find_index(elements, "fire")
-# test3 = find_index(elements, "earth")
+# find_index TEST    
+test1 = find_index(elements, "water")
+test2 = find_index(elements, "fire")
+test3 = find_index(elements, "dark matter")
 
 ## is_empty TEST    
 # test1 = is_empty(elements)
@@ -74,7 +84,7 @@ def all_same(items):
 # test3 = all_same(num_list)
     
 
-    
-# print(test1)
-# print(test2)
-# print(test3)
+
+print(test1)
+print(test2)
+print(test3)
